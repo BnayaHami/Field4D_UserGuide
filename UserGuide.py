a78ed7811f8b0c27185e1bf8e36bcad5f1b3a128
BnayaHami <123387055+BnayaHami@users.noreply.github.com>
2024-02-26 11:53:37 +0200

    Update UserGuide.py

--- UserGuide.py
+++ UserGuide.py
@@ -39,64 +39,64 @@ st.subheader("Let's get started")
 
 # footer
 
-import htbuilder
-from htbuilder import HtmlElement, div, ul, li, br, hr, a, p, img, styles, classes, fonts
-from htbuilder.units import percent, px
-from htbuilder.funcs import rgba, rgb
+# import htbuilder
+# from htbuilder import HtmlElement, div, ul, li, br, hr, a, p, img, styles, classes, fonts
+# from htbuilder.units import percent, px
+# from htbuilder.funcs import rgba, rgb
 
 
-def image(src_as_string, **style):
-    return img(src=src_as_string, style=styles(**style))
+# def image(src_as_string, **style):
+#     return img(src=src_as_string, style=styles(**style))
 
-def link(link, text, **style):
-    return a(_href=link, _target="_blank", style=styles(**style))(text)
+# def link(link, text, **style):
+#     return a(_href=link, _target="_blank", style=styles(**style))(text)
 
-def layout(*args):
+# def layout(*args):
 
-    style = """
-    <style>
-      # MainMenu {visibility: hidden;}
-      footer {visibility: hidden;}
-    </style>
-    """
+#     style = """
+#     <style>
+#       # MainMenu {visibility: hidden;}
+#       footer {visibility: hidden;}
+#     </style>
+#     """
 
-    style_div = styles(
-        left=0,
-        bottom=0,
-        margin=px(0, 0, 0, 0),
-        width=percent(100),
-        text_align="center",
-        height="0px",
-        opacity=0.8
-    )
+#     style_div = styles(
+#         left=0,
+#         bottom=0,
+#         margin=px(0, 0, 0, 0),
+#         width=percent(100),
+#         text_align="center",
+#         height="0px",
+#         opacity=0.8
+#     )
 
-    style_hr = styles(
-    )
+#     style_hr = styles(
+#     )
 
-    body = p()
-    foot = div(style=style_div)(hr(style=style_hr), body)
+#     body = p()
+#     foot = div(style=style_div)(hr(style=style_hr), body)
 
-    st.markdown(style, unsafe_allow_html=True)
+#     st.markdown(style, unsafe_allow_html=True)
 
-    for arg in args:
-        if isinstance(arg, str):
-            body(arg)
-        elif isinstance(arg, HtmlElement):
-            body(arg)
+#     for arg in args:
+#         if isinstance(arg, str):
+#             body(arg)
+#         elif isinstance(arg, HtmlElement):
+#             body(arg)
 
-    st.markdown(str(foot), unsafe_allow_html=True)
+#     st.markdown(str(foot), unsafe_allow_html=True)
 
-def footer():
-    myargs = [
-        "<b> GitHub <b>",
-        link("https://github.com/", image('https://i.pinimg.com/736x/b1/5e/ed/b15eedbdafbbdbca3249e3942f4faf3b.jpg',
-        	width=px(30), height=px(30), margin= "0em")),
-        " Our Website ",
-        link("https://streamlit.io/", image('https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX1523002.jpg',
-        	width=px(30), height=px(30), margin= "0em")),
-        br(),
-    ]
-    layout(*myargs)
+# def footer():
+#     myargs = [
+#         "<b> GitHub <b>",
+#         link("https://github.com/", image('https://i.pinimg.com/736x/b1/5e/ed/b15eedbdafbbdbca3249e3942f4faf3b.jpg',
+#         	width=px(30), height=px(30), margin= "0em")),
+#         " Our Website ",
+#         link("https://streamlit.io/", image('https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX1523002.jpg',
+#         	width=px(30), height=px(30), margin= "0em")),
+#         br(),
+#     ]
+#     layout(*myargs)
 
-if __name__ == "__main__":
-    footer()
+# if __name__ == "__main__":
+#     footer()
