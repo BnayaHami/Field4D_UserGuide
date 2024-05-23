cabdff4eb2cbeceeb1f257b6f6ead69189909dac
Bnaya Hami <bnayahami@gmail.com>
2024-05-23 15:48:20 +0300

    changes

--- pages/03_Hardware.py
+++ pages/03_Hardware.py
@@ -74,11 +74,11 @@ expander9 = st.expander('MicroSD')
 expander9.image(f'Hardware//sd.png', width=200)
 expander9.write('Including the Raspberry Pi Operating system')
 
-st.subheader('Texas Instruments CC2650stk SensorTag, Component Overview:')
+st.markdown('<p class="big-font"><b>Texas Instruments CC2650stk SensorTag, Component Overview:<b></p>', unsafe_allow_html=True)
 st.image(f'Hardware//sensortag.png')
-st.image(f'Hardware//sensortag2.png')
-st.image(f'Hardware//sensortag3.png')
 
+st.markdown('<p class="big-font"><b>A basic Illustration of the system<b></p>', unsafe_allow_html=True)
+st.image(f'Hardware//schema.png')
 
 
 # st.markdown("<style> .big-font {font-size:20px !important;color:black} </style>", unsafe_allow_html=True)
