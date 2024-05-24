e053882f537a9e30004fd377f263b8ea336ccf9e
Bnaya Hami <bnayahami@gmail.com>
2024-05-24 10:41:31 +0300

    links pages

--- pages/04_Firmware (Developers).py
+++ pages/04_Firmware (Developers).py
@@ -104,7 +104,7 @@ st.subheader('2. Write the LaunchPad and the SensorTag firmwares')
 st.write('**Download UniFlash software**')
 st.write('Link: https://www.ti.com/tool/UNIFLASH')
 st.write('If you are struggling with downloading, check the FAQ for more information')
-st.page_link("pages/07_FAQ.py", label="FAQ")
+st.page_link("pages/07_FAQ.py", label="Press here to go to FAQ")
 
 st.write('Note: You should get the LaunchPad and SensorTag firmwares from your provider')
 
@@ -241,9 +241,9 @@ with tab33:
     st.subheader("Your'e all set!")
 
 
-st.page_link("pages/06_Connect.py", label="Continue to Connect")
-st.markdown('Check out the Software section for additional information if needed')
+st.markdown('Note: Check out the Software section for additional information if needed')
 
+st.page_link("pages/06_Connect.py", label="Continue to Connect")
 
      
 
