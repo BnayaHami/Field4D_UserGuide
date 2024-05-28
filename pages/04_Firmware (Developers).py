fc1ed6229b6979390e5f0fe39b501fb866251bf6
Bnaya Hami <bnayahami@gmail.com>
2024-05-28 23:35:16 +0300

    firmware

--- pages/04_Firmware (Developers).py
+++ pages/04_Firmware (Developers).py
@@ -149,7 +149,6 @@ with tab16:
 
 with tab17:
      st.markdown('<p class="big-fontt"><b>Choose the "gw" file. Ensure that you select the "gw" file associated with the LaunchPad, not the "sp" file, which belongs to the SensorTag</b></p>', unsafe_allow_html=True)
-     st.subheader('**Choose the "gw" file. Ensure that you select the "gw" file associated with the LaunchPad, not the "sp" file, which belongs to the SensorTag**')
      st.image(f'Firmware/gw5.png')   
 
 with tab18:
@@ -219,7 +218,7 @@ with tab28:
      st.image(f'Firmware/sp6.png')
 
 with tab29:
-     st.subheader('**Choose the "sp" file. Ensure that you select the "sp" file associated with the SensorTag, not the "gw" file, which belongs to the LaunchPad**')
+     st.markdown('<p class="big-fontt"><b>Choose the "sp" file. Ensure that you select the "sp" file associated with the SensorTag, not the "gw" file, which belongs to the LaunchPad</b></p>', unsafe_allow_html=True)
      st.image(f'Firmware/sp7.png')
 
 with tab30:
