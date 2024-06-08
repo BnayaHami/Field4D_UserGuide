4b0ac1105ee968f1c1de47bcf185e328fca5df91
Bnaya Hami <bnayahami@gmail.com>
2024-06-08 22:18:20 +0300

    stl

--- pages/06_Connect.py
+++ pages/06_Connect.py
@@ -10,7 +10,7 @@ import base64
 
 
 # Your existing imports and code
-from stl import mesh
+from stl.mesh import Mesh
 from mpl_toolkits import mplot3d
 from matplotlib import pyplot as plt
 import plotly.graph_objects as go
