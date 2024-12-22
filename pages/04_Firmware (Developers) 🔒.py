eb621420a511279ed418385486d5be3ee3673b30
Bnaya Hami <bnayahami@gmail.com>
2024-12-22 13:49:16 +0200

    hardware

--- pages/04_Firmware (Developers) 🔒.py
+++ pages/04_Firmware (Developers) 🔒.py
@@ -17,8 +17,8 @@ if "authenticated_firmware" not in st.session_state:
 # Password protection logic
 if not st.session_state.authenticated_firmware:
     st.title("Restricted Access")
-    st.warning("This page is limited to developers only. Access is restricted.")
-    st.markdown("This section is not necessary for the installation, and refers to some guides and commands for developers.")
+    st.warning("This page is limited to developers only, Access is restricted")
+    st.markdown("This section is not necessary for the installation, and refers to some guides and commands for developers")
     password_input = st.text_input("Enter the password to access this page:", type="password")
     if st.button("Submit"):
         if password_input == PASSWORD:
